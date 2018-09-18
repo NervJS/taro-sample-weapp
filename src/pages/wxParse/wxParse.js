f86d648bde111d9fbcc9ac1e5244e08493284931
luckyadam <weitaozsh@gmail.com>
2018-09-18 22:00:00 +0800

    增加 TARO_ENV 控制 jsx 输出的示例

--- src/pages/wxParse/wxParse.js
+++ src/pages/wxParse/wxParse.js
@@ -1,9 +1,7 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 
-import WxParse from '../../components/wxParse/wxParse'
-
-import './wxParse.scss'
+import ParseComponent from './wxParseComponent'
 
 /**
  * 需要注意的是，在项目的 config/index.js 文件中，有 copy 模板与样式的操作
@@ -17,16 +15,10 @@ export default class WxParsePage extends Component {
     backgroundTextStyle: 'light'
   }
 
-  componentDidMount () {
-    const article = '<div style="color: red">我是HTML代码</div>'
-    WxParse.wxParse('article', 'html', article, this.$scope, 5)
-  }
-
   render () {
     return (
       <View>
-        <import src='../../components/wxParse/wxParse.wxml' />
-        <template is='wxParse' data='{{wxParseData:article.nodes}}'/>
+        {process.env.TARO_ENV === 'weapp' ? <ParseComponent /> : <View>只在小程序里支持</View>}
       </View>
     )
   }
