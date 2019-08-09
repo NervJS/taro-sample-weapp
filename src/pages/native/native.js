7d9d3379b9eccf8b584acc4b3b48bea9287bda34
chenfx <chenfx.cq@gmail.com>
2019-08-09 17:13:41 +0800

    增加一个原生页面调用taro组件的demo

--- src/pages/native/native.js
+++ src/pages/native/native.js
@@ -1,29 +1,46 @@
+/*
+ * @Description: Please Edit Description Info
+ * @Author: chenfx
+ * @Date: 2019-08-09 16:37:43
+ * @LastEditTime: 2019-08-09 17:02:17
+ */
 const { add } = require('../../utils/util')
 
 Page({
   data: {
     text: 'This is page data.',
-    x: add(1, 2)
+    x: add(1, 2),
+    taroText: 'This is taro component data......'
   },
-  created (options) {
+  created(options) {
     console.log(options)
     // Do some initialize when page load.
   },
-  onReady () {
+  onReady() {
     // console.log(this.selectComponent())
     // Do something when page ready.
   },
   // Event handler.
-  viewTap () {
+  // viewTap() {
+  //   debugger;
+  //   this.setData({
+  //     text: 'Set some data for updating view.'
+  //   }, function () {
+  //     // this is setData callback
+  //   })
+  // },
+
+  handler(e) {
+    console.log(e)
     this.setData({
       text: 'Set some data for updating view.'
-    }, function() {
-      // this is setData callback
     })
   },
-
-  handler (e) {
-    console.log(e)
+  taroHandler(e) {
+    console.log('---taro component trigger', e)
+    this.setData({
+      text: 'taro components click handle change text.....'
+    })
   },
   customData: {
     hi: 'MINA'
