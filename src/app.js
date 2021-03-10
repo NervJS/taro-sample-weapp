f82e124049d9450364add2c78912a69ce4801457
chenjiajian <798095202@qq.com>
2021-03-10 16:55:48 +0800

    chore: update to tarov3.1

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
-import Vue from 'vue'
 import { Block } from '@tarojs/components'
 
 // Vue.config.productionTip = false
 
-const App = new Vue({
+const App = {
   onShow (options) {
   },
   render(h) {
     // this.$slots.default 是将要会渲染的页面
     return h(Block, this.$slots.default)
   }
-})
+}
 
 export default App
